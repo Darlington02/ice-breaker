7d523455fc9f69a798c95422644a823efcb25c29
Darlington02 <darlingtonnnam@gmail.com>
2024-11-05 01:34:26 +0100

    chore: complete ice breaker with linkedin url

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,17 +2,17 @@ import os
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain_ollama import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 
 from third_parties.linkedin import scrape_linkedin_profile
-from tool.tools import get_profile_url_tavily
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 load_dotenv(override=True)
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-if __name__ == "__main__":
-    print("hello langchain!")
+def ice_break_with(name: str) -> str:
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_url)
 
     summary_template = """
             given the LinkedIn information {information} about a person from I want you to create:
@@ -25,9 +25,13 @@ if __name__ == "__main__":
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    # llm = ChatOllama(model="llama3.2")
     chain = summary_prompt_template | llm | StrOutputParser()
 
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/nnamdarlington/")
     res = chain.invoke(input={"information": linkedin_data})
-    print(get_profile_url_tavily("Darlington Nnam"))
+    print(res)
+
+
+if __name__ == "__main__":
+    print("Ice breaker!")
+    ice_break_with(name="Darlington Nnam")
+    
